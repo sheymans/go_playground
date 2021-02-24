edd43e937e2a337ceaf33da487ed2e5a5afe8d1c
Stijn Heymans <stijn@ziprecruiter.com>
2021-02-24 09:34:22 -0800

    Add toy code for Chapter 3 Introducing Go

--- main.go
+++ main.go
@@ -8,4 +8,19 @@ func main() {
 	fmt.Println("1.0 + 1.0 = ", 1.0 + 1.0)
 	fmt.Println("Hello"[1]) // shows 101 instead of e: it prints the byte for the character e.
 	fmt.Println(true && true)
+
+	const x string = "Hello"
+	fmt.Println(x)
+	y := "world"
+	fmt.Println(x == y)
+
+	double()
+}
+
+func double() {
+	fmt.Print("Enter a number: ")
+	var input float64
+	fmt.Scanf("%f", &input)
+	output := input * 2
+	fmt.Println(output)
 }
